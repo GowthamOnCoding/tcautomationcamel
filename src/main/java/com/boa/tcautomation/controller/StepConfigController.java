5a989bec19c542072a29d6218575c3436df17a58
gowthamoncoding <vm.gowtham@hotmail.com>
2025-02-04 09:25:06 +0530

    ui changes

--- src/main/java/com/boa/tcautomation/controller/StepConfigController.java
+++ src/main/java/com/boa/tcautomation/controller/StepConfigController.java
@@ -2,6 +2,8 @@ package com.boa.tcautomation.controller;
 
 import com.boa.tcautomation.model.StepConfig;
 import com.boa.tcautomation.repository.StepConfigRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 @Controller
 public class StepConfigController {
 
     @Autowired
     private StepConfigRepository stepConfigRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(StepConfigController.class);
 
     @GetMapping("/stepconfig/add")
     public String showAddStepConfigForm(Model model) {
@@ -45,5 +50,13 @@ public class StepConfigController {
     public String updateStepConfig(@ModelAttribute StepConfig stepConfig) {
         stepConfigRepository.save(stepConfig);
         return "redirect:/stepconfig/list";
+    }
+
+    @GetMapping("/stepconfigs")
+    public String listAllStepConfigs(Model model) {
+        logger.info("Listing all step configs");
+        model.addAttribute("stepConfigs", stepConfigRepository.findAll());
+        logger.info("Returning stepconfig/list {}",stepConfigRepository.findAll());
+        return "stepconfig/list";
     }
 }
